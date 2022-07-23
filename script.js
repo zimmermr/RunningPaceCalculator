3a65fcbde4574fd7a1f372104d05e3caefa5347c
Michael Zimmerman <65474223+zimmermr@users.noreply.github.com>
2022-07-22 22:49:47 -0400

    Update via upload
    Finished most of the conversion between time format and total second calculations. Still having issues anytime the hours input isn't filled out.

--- script.js
+++ script.js
@@ -7,67 +7,87 @@ const btnReset = document.getElementById('btnReset');
 const fullForm = document.getElementById('previousTime');
 
 let timeInSeconds = 0;
+let hours, minutes, seconds;
 
 btnCalc.addEventListener('click', calculate);
 btnReset.addEventListener('click', clear);
 
 function calculate() {
+  getNumbersFromInputs();
   if (checkTimeInputs()) {
-    convertToSeconds(hh.value, mm.value, ss.value);
+    convertToSeconds(hours, minutes, seconds);
     msg.textContent = '';
     addMessage(`Your time in seconds is ${timeInSeconds}`);
-    addMessage(`Converted to time format: ${convertToTimeFormat(convertToSeconds(hh.value, mm.value, ss.value))}`)
+    addMessage(`\nConverted to time format: ${convertToTimeFormat(convertToSeconds(hours, minutes, seconds))}`)
   }
 }
 
 function checkTimeInputs() {
-  if (hh.value > 0 && hh.value < 25 && mm.value < 60 && mm.value > 0 && ss.value > 0 && ss.value < 60) {
+  let validNumbers = hours > 0 && hours < 25 && minutes < 60 && minutes > 0 && seconds > 0 && seconds < 60;
+  if (validNumbers || !hh.value) {
     return true;
   } else {
     clearMessage();
-    if (hh.value < 0) {
-      addMessage(`${hh.value} hours? Please show me your time machine!\n`)
+    if (hours < 0) {
+      addMessage(`${hours} hours? Please show me your time machine!\n`)
     }
 
-    if (hh.value > 23) {
-      addMessage(`${hh.value} hours? I can't compute races that take longer than a day.`)
+    if (hours > 23) {
+      addMessage(`${hours} hours? I can't compute races that take longer than a day.`)
     }
 
-    if (mm.value > 59 || mm.value < 0) {
-      addMessage(`${mm.value} minutes? That ain't right. Enter a number from 0 to 59.`)
+    if (minutes > 59 || minutes < 0) {
+      addMessage(`${minutes} minutes? That ain't right. Enter a number from 0 to 59.`)
     }
 
-    if (ss.value > 59 || ss.value < 0) {
-      addMessage(`${ss.value} seconds? Impossible! Enter a number from 0 to 59.`)
+    if (seconds > 59 || seconds < 0) {
+      addMessage(`${seconds} seconds? Impossible! Enter a number from 0 to 59.`)
     }
     return false;
   }
 }
 
-// FIX THIS
-function convertToSeconds(hours, minutes, seconds) {
-  if (hours === null || hours < 1) {
-    timeInSeconds = (minutes * 60) + seconds;
-  } else if (minutes === null) {
+function convertToSeconds(h, m, s) {
+  if (!h || hours < 1) {
+    timeInSeconds = (m * 60) + s;
+  } else if (!m) {
     addMessage('Minutes required.');
     return false;
   } else {
-    timeInSeconds = (hours * 3600) + (minutes * 60) + (seconds * 1);
+    timeInSeconds = (h * 3600) + (m * 60) + s;
     return timeInSeconds;
   }
-
 }
 
-// FIX THIS
+// Add extra zeroes -- NUMBERS STILL AREN'T SHOWING UP CORRECTLY
 function convertToTimeFormat(totalSeconds) {
+  let hoursFromSec, minutesFromSec, secondsFromSec;
+
   if (totalSeconds >= 3600) {
-    timeFormat = `${Math.trunc(totalSeconds / 3600)}:${Math.trunc((totalSeconds % 3600) / 60)}:${Math.trunc((totalSeconds % 3600) % 60)}`;
+    hoursFromSec = Math.trunc(totalSeconds / 3600);
+    minutesFromSec = Math.trunc((totalSeconds % 3600) / 60);
+    secondsFromSec = Math.trunc((totalSeconds % 3600) % 60);
+
+    timeFormat = `${hoursFromSec}:` +
+      `${minutesFromSec < 10 ? '0' + minutesFromSec : minutesFromSec}:` +
+      `${secondsFromSec < 10 ? '0' + secondsFromSec : secondsFromSec}`;
   } else if (totalSeconds >= 60) {
-    timeFormat = `${Math.trunc(totalSeconds / 60)}:${totalSeconds % 60}}`;
+    minutesFromSec = Math.trunc(totalSeconds / 60);
+    secondsFromSec = totalSeconds % 60;
+
+    timeFormat = `${minutesFromSec}:` +
+      `${secondsFromSec < 10 ? '0' + secondsFromSec : secondsFromSec}`;
   } else {
     timeFormat = `00:${totalSeconds}`;
   }
   return timeFormat;
+}
+
+// Makes sure the inputs are converted to numbers
+function getNumbersFromInputs() {
+  hours = Number(hh.value);
+  minutes = Number(mm.value);
+  seconds = Number(ss.value);
 }
 
 function addMessage(message) {
